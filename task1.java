2775bd7b497dca9f7be3d4a4acd52d5df63b85b6
aghafoor1208 <52696487+aghafoor1208@users.noreply.github.com>
2025-01-16 14:58:33 +0500

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,11 @@
+class Hello{
+
+public static void main(String[] args){
+    System.out.println("Abdul Ghafoor");
+    System.out.println("Lecturer AI and MMG");
+    System.out.print("Aror University");
+    
+    
+
+}
+}
